2d0e6259e5602403e984d6efcb7ea787087a6d96
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2026-07-01 01:43:52 +0800

    Use Chinese version of xlsx

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from htmltools._core import Tag
-from shiny import App, ui, reactive, render
-from shinywidgets import output_widget, render_widget  
+import os
+from shiny import App, ui, reactive
+from shinywidgets import output_widget, render_widget
 
 import polars as pl
-import io
 
-from data import get_data, fetch_raw_data
-from i18n import i18n
+from data import get_data
+from i18n import i18n, set_language
 import plotly.express as px
-
-df = get_data()
 
 
 def apply_plot_interaction_defaults(fig):
@@ -33,10 +30,31 @@ app_ui = ui.page_fluid(
 
 
 def server(input, output, session):
+
+   @reactive.calc
+   def lang():
+       query = session.clientdata.url_search()
+       params = {}
+       if query.startswith("?"):
+           query = query[1:]
+       for pair in query.split("&"):
+           if "=" in pair:
+               k, v = pair.split("=", 1)
+               params[k] = v
+       # Query param takes precedence; fall back to env var; ultimate default CN
+       return params.get("lang") or os.getenv("LANGUAGE", "CN")
+
+   @reactive.calc
+   def df():
+       """Load the language-appropriate dataset (cached per language)."""
+       return get_data(lang())
+
    @render_widget # pyrefly: ignore
    def time_plot():
+      set_language(lang())
+      data = df()
       freq = (
-         df.group_by("Announcement_Year")
+         data.group_by("Announcement_Year")
          .agg(pl.count().alias("frequency"))
          .sort("Announcement_Year")
       )
@@ -56,10 +74,13 @@ def server(input, output, session):
          paper_bgcolor="white"
       )
       return apply_plot_interaction_defaults(fig)
+
    @render_widget # pyrefly: ignore
    def category_plot():
+      set_language(lang())
+      data = df()
       category_freq = (
-         df.group_by("Target_Category")
+         data.group_by("Target_Category")
          .agg(pl.count().alias("frequency"))
          .sort("frequency")
       )
